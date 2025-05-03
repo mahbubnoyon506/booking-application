ad9f6c44cd69d5d44986392091ac2092ee23e370
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-05-03 17:51:52 +0600

    Build issue fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -45,6 +45,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/my-hotels", hotelRoutes);
 
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+// });
+
 // server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
--- frontend/src/forms/ManageHotelForm/TypeSection.tsx
+++ frontend/src/forms/ManageHotelForm/TypeSection.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useFormContext } from "react-hook-form";
 import { hotelTypes } from "../../config/hotel-options-config";
 import { HotelFormData } from "./ManageHotelForm";
