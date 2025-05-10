9aca37b6001b6b1adf621a4be46f42cd1b31197a
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-05-10 18:14:45 +0600

    Hotel details page created

--- frontend/src/pages/HotelDetails.tsx
+++ frontend/src/pages/HotelDetails.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
 import { useParams } from "react-router-dom";
 import { getHotelById } from "../api-clients";
+import Loader from "../components/Loader";
+import { AiFillStar } from "react-icons/ai";
+import GuestInfoForm from "../forms/GuestInfoForm";
 
 const HotelDetails = () => {
   const { hotelId } = useParams();
@@ -14,7 +16,54 @@ const HotelDetails = () => {
   });
   console.log(hotel);
 
-  return <div>HotelDetails</div>;
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="space-y-6">
+          <div>
+            <span className="flex">
+              {Array.from({ length: hotel.starRating }).map(() => (
+                <AiFillStar className="fill-yellow-400" />
+              ))}
+            </span>
+            <h1 className="text-3xl font-bold">{hotel.name}</h1>
+          </div>
+
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {hotel?.imageURLs?.map((image) => (
+              <div className="h-[300px]">
+                <img
+                  src={image}
+                  alt={hotel.name}
+                  className="rounded-md w-full h-full object-cover object-center"
+                />
+              </div>
+            ))}
+          </div>
+
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-2">
+            {hotel.facilities.map((facility) => (
+              <div className="border border-slate-300 rounded-sm p-3">
+                {facility}
+              </div>
+            ))}
+          </div>
+
+          <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr]">
+            <div className="whitespace-pre-line">{hotel.description}</div>
+            <div className="h-fit">
+              <GuestInfoForm
+                pricePerNight={hotel.pricePerNight}
+                hotelId={hotel._id}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default HotelDetails;
