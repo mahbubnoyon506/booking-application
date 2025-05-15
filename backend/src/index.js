dd11c8c48cb33aaf901be4dff49d38039ed5f567
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-05-16 00:44:56 +0600

    Variable update

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,12 +25,21 @@ const PORT = process.env.PORT || 7000;
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const allowedOrigins = process.env.FRONTEND_URLS?.split(",") || [];
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_SECOND],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 
 // database connection
