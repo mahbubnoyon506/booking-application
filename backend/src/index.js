35973af4c020d17358b67c63be1043b5bd775485
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-05-16 00:15:30 +0600

    Domain updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_SECOND],
     credentials: true,
   })
 );
